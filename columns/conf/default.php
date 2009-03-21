aef51db13e368d85641a9905ef464d3cbedd4793
ostrovsm <ostrovsm@709dd3f2-9485-11dd-9c30-33875a484c2d>
2009-03-21 19:45:22 +0000

    Namespace style inheritance implementation

--- columns/conf/default.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Configuration defaults
- *
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Mykola Ostrovskyy <spambox03@mail.ru>
- */
-
-$conf['wrapnewcol'] = 1;
