5390b66686a6125865931bc6056a2b3711bfa493
ostrovsm <ostrovsm@709dd3f2-9485-11dd-9c30-33875a484c2d>
2009-09-01 17:33:40 +0000

    Development of reference database system

---
+++ branches/refnotes-reference_database/config.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Plugin RefNotes: Configuration
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Mykola Ostrovskyy <spambox03@mail.ru>
+ */
+
+function refnotes_loadConfigFile($name) {
+    $pluginRoot = DOKU_PLUGIN . 'refnotes/';
+    $fileName = $pluginRoot . $name . '.local.dat';
+
+    if (!file_exists($fileName)) {
+        $fileName = $pluginRoot . $name . '.dat';
+        if (!file_exists($fileName)) {
+            $fileName = '';
+        }
+    }
+
+    if ($fileName != '') {
+        $result = unserialize(io_readFile($fileName, false));
+    }
+    else {
+        $result = array();
+    }
+
+    return $result;
+}
+
+function refnotes_saveConfigFile($name, $config) {
+    $pluginRoot = DOKU_PLUGIN . 'refnotes/';
+    $fileName = $pluginRoot . $name . '.local.dat';
+
+    return io_saveFile($fileName, serialize($config));
+}
---
+++ branches/refnotes-reference_database/info.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Plugin RefNotes: Information
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Mykola Ostrovskyy <spambox03@mail.ru>
+ */
+
+function refnotes_getInfo($component = '') {
+    $info = array(
+        'author' => 'Mykola Ostrovskyy',
+        'email'  => 'spambox03@mail.ru',
+        'date'   => '2009-08-01',
+        'name'   => 'RefNotes Plugin',
+        'desc'   => 'Extended syntax for footnotes and references.',
+        'url'    => 'http://code.google.com/p/dwp-forge/',
+    );
+
+    if ($component != '') {
+        if (($_REQUEST['do'] == 'admin') && !empty($_REQUEST['page']) && ($_REQUEST['page'] == 'plugin')) {
+            $info['name'] .= ' (' . $component . ')';
+        }
+    }
+
+    return $info;
+}
